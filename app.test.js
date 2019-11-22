aeaea5c0589374d844dffaffa0c5355ae7f2d184
Renaud Racinet <renaud.racinet@estiam.com>
2019-11-22 18:26:56 +0100

    test(app) pass seven tests

--- app.test.js
+++ app.test.js
@@ -19,4 +19,27 @@ describe('Counting Duplicates', () => {
 		expect(app.countingDuplicates(received)).toBe(expected);
 	});
 
+	it('should indivisibility return nothing', () => {
+		expected = 1;
+		received = 'indivisibility';
+		expect(app.countingDuplicates(received)).toBe(expected);
+	});
+
+	it('should indivisibilities return nothing', () => {
+		expected = 2;
+		received = 'indivisibilities';
+		expect(app.countingDuplicates(received)).toBe(expected);
+	});
+
+	it('should indivisibilities return nothing', () => {
+		expected = 2;
+		received = 'indivisibilities';
+		expect(app.countingDuplicates(received)).toBe(expected);
+	});
+
+	it('should aA11 return nothing', () => {
+		expected = 2;
+		received = 'aA11';
+		expect(app.countingDuplicates(received)).toBe(expected);
+	});
 });
